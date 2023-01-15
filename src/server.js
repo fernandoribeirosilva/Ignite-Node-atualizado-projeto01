7a7919279c43e9cd2a3bd974ac806e9aeadb4f64
fernandoribeirosilva <fernando1996ribeirosilva@gmail.com>
2023-01-15 09:10:52 -0300

    delete

--- src/server.js
+++ src/server.js
@@ -3,13 +3,24 @@ import http from "node:http";
 import { json } from "./middlewares/json.js";
 import { routes } from "./routes.js";
 
+// Query Parameters: URL Stateful => filtros, paginação
+//                             query parameters
+// http://localhost:30000/users?userId=1
+
+// Router Parameters: Identificação de recurso
+// GET http://localhost:30000/users/1
+// DELETE http://localhost:30000/users/1
+
+// Request Body: Envio de informações de formulário (HTTPs)
+// POST http://localhost:30000/users
+
 const server = http.createServer(async (req, res) => {
   const { method, url } = req;
 
   await json(req, res)
 
   const route = routes.find(route => {
-    return route.method === method && route.path === url;
+    return route.method === method && route.path.test(url);
   });
 
   if (route) {
